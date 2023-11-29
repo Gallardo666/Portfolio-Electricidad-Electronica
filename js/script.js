e4c932c809be99f26eb5bc2313d96666e1e26e95
Jordi Gallardo <jordigallardo0621@gmail.com>
2023-11-29 15:26:41 +0100

    Ajustado Portfolio Electricidad (falta actualizar fotos y CV)

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin:'right' });
 
 /*======================= typed js =======================*/
 const typed =new Typed ('.multiple-text', {
-    strings: ['Programador','Diseñador Grafico','YouTuber'],
+    strings: ['Electricista','Programador','Electrónico'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:100,
